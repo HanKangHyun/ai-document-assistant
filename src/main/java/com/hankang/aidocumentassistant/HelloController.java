355470602546917e07bd8c4f1c84fe722c2fc089
Kanghyun Han <gksrkd0307@gmail.com>
2026-08-31 16:22:05 +0900

    Initial Spring Boot setup

---
+++ src/main/java/com/hankang/aidocumentassistant/HelloController.java
@@ -0,0 +1,13 @@
+package com.hankang.aidocumentassistant;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello AI Document Assistant";
+    }
+}
---
+++ src/test/java/com/hankang/aidocumentassistant/AiDocumentAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hankang.aidocumentassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDocumentAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
